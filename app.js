1327bedacc994a214205e36e1376a46c62158ec5
CokeMine <aptx4561@gmail.com>
2021-02-05 00:35:28 +0800

    Init

---
+++ app.js
@@ -0,0 +1,39 @@
+const Koa = require('koa');
+const views = require('koa-views');
+const serve = require('koa-static');
+const bodyParser = require('koa-bodyparser');
+const path = require('path');
+const router = require('./router')
+const Resolve = p => path.resolve(__dirname, p);
+const app = new Koa();
+const debug = () => {
+    return async (ctx, next) => {
+        try {
+            await next();
+        } catch (e) {
+            if (e.response) {
+                ctx.body = {
+                    status: e.response.status,
+                    message: e.response.data
+                }
+            } else if (e.request) {
+                ctx.body = {
+                    status: 500,
+                    message: "请求GitHub API发生错误"
+                }
+            } else {
+                ctx.body = e;
+            }
+        }
+    }
+}
+app.use(debug());
+app.use(bodyParser());
+app.use(serve(Resolve('./static')));
+app.use(views((Resolve('./views')), {
+    extension: 'ejs'
+}));
+app.use(router.routes());
+app.listen(3000, () => {
+    console.log("http://localhost:3000");
+})
---
+++ router.js
@@ -0,0 +1,98 @@
+const Router = require('koa-router');
+const axios = require('axios');
+const router = new Router();
+const {originationName, orgUserName, Oauth, userName, personalToken} = require('./config.json');
+const {clientID, clientSecret} = Oauth;
+const auth = {};
+router.get('/', async (ctx, next) => {
+    await ctx.render('default', {
+        originationName
+    });
+    await next();
+});
+router.get('/github/login', async ctx => {
+    let path = `https://github.com/login/oauth/authorize?client_id=${clientID}`;
+    ctx.redirect(path);
+});
+router.get('/oauth/redirect', async ctx => {
+    const requestToken = ctx.request.query.code;
+    let res = await axios.post('https://github.com/login/oauth/access_token', {
+        "client_id": clientID,
+        "client_secret": clientSecret,
+        "code": requestToken
+    }, {
+        "headers": {
+            "Accept": "application/json"
+        }
+    });
+    if (res.data["error"]) {
+        throw({status: 500, message: "出现了一些未知错误"});
+        //{
+        //   error: 'bad_verification_code',
+        //   error_description: 'The code passed is incorrect or expired.',
+        //   error_uri: 'https://docs.github.com/apps/managing-oauth-apps/troubleshooting-oauth-app-access-token-request-errors/#bad-verification-code'
+        // }
+        //{
+        //   access_token: '',
+        //   token_type: 'bearer',
+        //   scope: ''
+        // }
+    } else {
+        const accessToken = res.data["access_token"];
+        res = await axios.get('https://api.github.com/user', {
+            "headers": {
+                "Authorization": `token ${accessToken}`
+            }
+        });
+        let {login, avatar_url, name, id} = res.data;
+        if (!name) name = login;
+        if (!auth[login]) auth[login] = {}
+        auth[login]["requestToken"] = requestToken;
+        auth[login]["id"] = id;
+        await ctx.render('default', {
+            originationName,
+            avatar_url,
+            name,
+            login
+        });
+    }
+});
+router.get('/join', async (ctx, next) => {
+    const referer = ctx.request.header['referer'];
+    const login = ctx.request.query.login;
+    if (!referer || !login) {
+        throw({
+            status: 403,
+            message: "再检查一下？"
+        });
+    }
+    const myURL = new URL(referer);
+    const code = myURL.searchParams.get('code');
+    if (!code || !myURL || auth[login]["requestToken"] !== code) {
+        throw({
+            status: 403,
+            message: "再检查一下？"
+        });
+    }
+    if (auth[login]["status"]) {
+        throw({
+            status: 403,
+            message: "已经邀请过你啦！"
+        })
+    }
+    const res = await axios.post(`https://api.github.com/orgs/${orgUserName}/invitations`, {
+        "invitee_id": auth[login]["id"],
+        "role": "direct_member"
+    }, {
+        headers: {
+            "Accept": "application/vnd.github.v3+json",
+        },
+        auth: {
+            username: userName,
+            password: personalToken
+        }
+    });
+    auth[login]["status"] = true;
+    await next();
+});
+module.exports = router
